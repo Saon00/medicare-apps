495f52c89e68058fcae80243902cf46ee30ed063
Saon00 <sikdersaon1@gmail.com>
2023-04-20 01:29:08 +0600

    top doctors added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -99,90 +99,174 @@ class _HomePageState extends State<HomePage> {
             Text("Top Doctors", style: GoogleFonts.poppins(fontSize: 20)),
             const SizedBox(height: 5),
             SingleChildScrollView(
-              child: Row(children: [
-                GestureDetector(
-                  child: Container(
-                    color: Colors.white,
-                    height: 150,
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              // rating
-                              Row(
-                                children: [
-                                  Icon(
-                                    Icons.star,
-                                    color: Colors.yellow.shade700,
-                                    size: 20,
-                                  ),
-                                  Icon(
-                                    Icons.star,
-                                    color: Colors.yellow.shade700,
-                                    size: 20,
-                                  ),
-                                  Icon(
-                                    Icons.star,
-                                    color: Colors.yellow.shade700,
-                                    size: 20,
-                                  ),
-                                  Icon(
-                                    Icons.star,
-                                    color: Colors.yellow.shade700,
-                                    size: 20,
-                                  ),
-                                  Icon(
-                                    Icons.star_half,
-                                    color: Colors.yellow.shade700,
-                                    size: 20,
-                                  ),
-                                  const SizedBox(width: 10),
-                                  Text(
-                                    "4.5",
-                                    style: GoogleFonts.poppins(fontSize: 15),
-                                  )
-                                ],
-                              ),
-                              const SizedBox(height: 5),
-
-                              // name
-                              Text('Dr. FirstName',
-                                  style: GoogleFonts.ubuntu(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 20)),
-                              Text('LastName',
-                                  style: GoogleFonts.ubuntu(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 20)),
-                              const SizedBox(height: 10),
-
-                              //category icon & name
-                              Row(
-                                children: [
-                                  Image.asset("assets/categoryIcons/heart.png",
-                                      height: 20),
-                                  const SizedBox(width: 5),
-                                  Text(
-                                    'Heart',
-                                    style: GoogleFonts.ubuntu(fontSize: 17),
-                                  )
-                                ],
-                              )
-                              // button
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: const [
+                  TopDoctorsLits(
+                    firstName: 'Dr. Robert',
+                    lastName: 'Alber',
+                    docCategoryName: 'Brain',
+                    imgPath: 'assets/categoryIcons/brain.png',
+                    rating: '4.5',
+                    docImgpath: 'assets/doctorIcons/doctor01.png',
                   ),
-                ),
-              ]),
+                  SizedBox(width: 20),
+                  TopDoctorsLits(
+                    firstName: 'Dr. Ana',
+                    lastName: 'Aslan',
+                    docCategoryName: 'Pregnancy',
+                    imgPath: 'assets/categoryIcons/pregnant.png',
+                    rating: '4.5',
+                    docImgpath: 'assets/doctorIcons/doctor02.png',
+                  ),
+                  SizedBox(width: 20),
+                  TopDoctorsLits(
+                    firstName: 'Dr. John',
+                    lastName: 'kaber',
+                    docCategoryName: 'Dental',
+                    imgPath: 'assets/categoryIcons/teeth.png',
+                    rating: '4.5',
+                    docImgpath: 'assets/doctorIcons/doctor03.png',
+                  ),
+                  SizedBox(width: 20),
+                  TopDoctorsLits(
+                    firstName: 'Dr. Rubina',
+                    lastName: 'Sharmin',
+                    docCategoryName: 'Blood',
+                    imgPath: 'assets/categoryIcons/blood.png',
+                    rating: '4.5',
+                    docImgpath: 'assets/doctorIcons/doctor04.png',
+                  ),
+                ],
+              ),
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class TopDoctorsLits extends StatelessWidget {
+  final String firstName;
+  final String lastName;
+  final String rating;
+  final String docCategoryName;
+  final String docImgpath;
+  final String imgPath;
+
+  const TopDoctorsLits({
+    Key? key,
+    required this.firstName,
+    required this.lastName,
+    required this.rating,
+    required this.docCategoryName,
+    required this.imgPath,
+    required this.docImgpath,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(10)),
+      height: 120,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(5),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // rating
+                Row(
+                  children: [
+                    Icon(
+                      Icons.star,
+                      color: Colors.yellow.shade700,
+                      size: 10,
+                    ),
+                    Icon(
+                      Icons.star,
+                      color: Colors.yellow.shade700,
+                      size: 10,
+                    ),
+                    Icon(
+                      Icons.star,
+                      color: Colors.yellow.shade700,
+                      size: 10,
+                    ),
+                    Icon(
+                      Icons.star,
+                      color: Colors.yellow.shade700,
+                      size: 10,
+                    ),
+                    Icon(
+                      Icons.star_half,
+                      color: Colors.yellow.shade700,
+                      size: 10,
+                    ),
+                    const SizedBox(width: 10),
+                    Text(
+                      rating,
+                      style: GoogleFonts.poppins(fontSize: 10),
+                    )
+                  ],
+                ),
+                const SizedBox(height: 5),
+
+                // name
+                Text(firstName,
+                    style: GoogleFonts.ubuntu(
+                        fontWeight: FontWeight.bold, fontSize: 15)),
+                Text(lastName,
+                    style: GoogleFonts.ubuntu(
+                        fontWeight: FontWeight.bold, fontSize: 15)),
+                const SizedBox(height: 10),
+
+                //category icon & name
+                Row(
+                  children: [
+                    Image.asset(imgPath, height: 15),
+                    const SizedBox(width: 5),
+                    Text(
+                      docCategoryName,
+                      style: GoogleFonts.ubuntu(fontSize: 13),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Column(
+            children: [
+              Image.asset(
+                docImgpath,
+                height: 60,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 35,
+                  width: 35,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.blue,
+                  ),
+                  child: IconButton(
+                    icon: const Icon(
+                      Icons.arrow_forward_rounded,
+                      size: 20,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {},
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
